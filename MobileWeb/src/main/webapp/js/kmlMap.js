9859c96b113edbaf9aec2fbeab680ca9c41cebee
Mike Jones <mike.a.jones@bristol.ac.uk>
2009-08-28 17:46:11 +0100

    if the error callback is called it will display the default map

--- MobileWeb/src/main/webapp/js/kmlMap.js
+++ MobileWeb/src/main/webapp/js/kmlMap.js
@@ -1,49 +1,49 @@
 /**
 
-This script performs a number of operations:
+ This script performs a number of operations:
 
-1) Displays map data obtained from a KML file provided by a URL.
-2) Uses the geolocation API to display the current position.
+ 1) Displays map data obtained from a KML file provided by a URL.
+ 2) Uses the geolocation API to display the current position.
 
-The W3C geolocation specification is used to display the location of the user viewing the map.
-However, a number of things need to be taken into consideration:
+ The W3C geolocation specification is used to display the location of the user viewing the map.
+ However, a number of things need to be taken into consideration:
 
-1) The device might not support the geolocation specification.
-2) The user might not want their location revealed.
-3) The device is not providing an accurate location.
-4) The user is not near the campus.
+ 1) The device might not support the geolocation specification.
+ 2) The user might not want their location revealed.
+ 3) The device is not providing an accurate location.
+ 4) The user is not near the campus.
 
-It was also clear that an iPhone would not give an accurate location with a one-shot
-location request. The script therefore will poll a number of times until it has
-either received a location within set accuracy level (goodAccuracy) or it had reached
-a maximum number of poll requests (maxPoll).
+ It was also clear that an iPhone would not give an accurate location with a one-shot
+ location request. The script therefore will poll a number of times until it has
+ either received a location within set accuracy level (goodAccuracy) or it had reached
+ a maximum number of poll requests (maxPoll).
 
-If the device's current location is a set distance from the campus (maxDistance) then
-the map will centre on the default campus coordinates (defaultLatitude, defaultLongitude).
-If the accuracy is poor (value greater than that set in 'moderateAccuracy') then
-the map will centre on the default campus coordinates. If the accuracy level is moderate
-(moderateAccuracy) then a warning about accuracy is given and the map will centre on
-the device's location. If we have good accuracy (goodAccuracy) the map will centre
-on the device's location without a warning.
+ If the device's current location is a set distance from the campus (maxDistance) then
+ the map will centre on the default campus coordinates (defaultLatitude, defaultLongitude).
+ If the accuracy is poor (value greater than that set in 'moderateAccuracy') then
+ the map will centre on the default campus coordinates. If the accuracy level is moderate
+ (moderateAccuracy) then a warning about accuracy is given and the map will centre on
+ the device's location. If we have good accuracy (goodAccuracy) the map will centre
+ on the device's location without a warning.
 
-If the device doesn't support the API or the user disallows access, then the
-map will centre on the default campus coordinates.
+ If the device doesn't support the API or the user disallows access, then the
+ map will centre on the default campus coordinates.
 
-Expected parameter values:
+ Expected parameter values:
 
-mapElementId			the ID of the HTML element to hold the map
-defaultLatitude			fallback latitude coordinate
-defaultLongitude		fallback longitude coordinate
-maxPoll					maximum number of times we will try and get an accurate location
-goodAccuracy			maximum value (meters) we view as good for accuracy
-moderateAccuracy		maximum value (meters) we view as moderate accuracy
-maxDistance				maximum distance (meters) from the default location that is acceptable
-defaultZoomLevel		zoomLevel used in google maps
-kmlUrl					URL of a kml file
+ mapElementId            the ID of the HTML element to hold the map
+ defaultLatitude            fallback latitude coordinate
+ defaultLongitude        fallback longitude coordinate
+ maxPoll                    maximum number of times we will try and get an accurate location
+ goodAccuracy            maximum value (meters) we view as good for accuracy
+ moderateAccuracy        maximum value (meters) we view as moderate accuracy
+ maxDistance                maximum distance (meters) from the default location that is acceptable
+ defaultZoomLevel        zoomLevel used in google maps
+ kmlUrl                    URL of a kml file
 
-Author: Mike Jones (mike.a.jones@bristol.ac.uk)
+ Author: Mike Jones (mike.a.jones@bristol.ac.uk)
 
-**/
+ **/
 var initializeMap = function(mapElementId, defaultLatitude, defaultLongitude, maxPoll, goodAccuracy, moderateAccuracy, maxDistance, defaultZoomLevel, kmlUrl) {
 
     google.load("maps", "2.x");
@@ -109,8 +109,9 @@ var initializeMap = function(mapElementId, defaultLatitude, defaultLongitude, ma
         }
 
         function errorCallBack(error) {
-            alert(error.message);
             navigator.geolocation.clearWatch(watchId);
+            hideSearchMessage();
+            displayMap(defaultLatitude, defaultLongitude);
         }
 
         function displayMap(latitude, longitude) {
