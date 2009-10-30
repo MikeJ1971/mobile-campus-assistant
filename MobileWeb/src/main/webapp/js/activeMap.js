0b478ba14a4a24b3f832054f89ca4131537be436
Jasper Tredgold <jasper.tredgold@bris.ac.uk>
2009-10-30 11:49:16 +0000

    Display of stops tracks visible viewport.

--- MobileWeb/src/main/webapp/js/activeMap.js
+++ MobileWeb/src/main/webapp/js/activeMap.js
@@ -44,9 +44,10 @@
  Author: Mike Jones (mike.a.jones@bristol.ac.uk)
 
  **/
-var initializeMap = function(mapElementId, defaultLatitude, defaultLongitude, maxPoll, goodAccuracy, moderateAccuracy, maxDistance, defaultZoomLevel, proxyUrl, icon, markers) {
+var initializeMap = function(mapElementId, defaultLatitude, defaultLongitude, maxPoll, goodAccuracy, moderateAccuracy, maxDistance, defaultZoomLevel, proxyUrl, icon, markerData) {
 
 	var map;
+	var markers = new Array(); // holds stop markers
 	
 	window.onload = function() {
 	
@@ -55,17 +56,77 @@ var initializeMap = function(mapElementId, defaultLatitude, defaultLongitude, ma
 		      zoom: defaultZoomLevel,
 		      center: latlng,
 		      disableDefaultUI: true,
+		      disableDoubleClickZoom: true,
+			  keyboardShortcuts: false,
+			  scrollwheel: false,
 //		      navigationControl: true,
 		      mapTypeId: google.maps.MapTypeId.ROADMAP
 		};
 		map = new google.maps.Map(document.getElementById(mapElementId), myOptions);
+		
+		// populate the markers array
+		createMarkers();
 
-		overlayMarkers();
+		// we use the loading of viewable tiles to trigger the refreshing of the markers
+		google.maps.event.addListener(map, 'tilesloaded', function() {
+			overlayMarkers();
+		});
+
 		findLocation();
-	
+
 	}
 	
-    findLocation = function() {
+	// intialise the markers
+    var createMarkers = function() {
+    	
+    	var infowindow = new google.maps.InfoWindow();
+    	
+    	for(var i=0; i<markerData.length; i++) {
+        	var point = new google.maps.LatLng(markerData[i].lat, markerData[i].lng);
+            var markerId = markerData[i].id;
+            var marker = new google.maps.Marker({
+                position: point, 
+                icon: icon
+            });
+            
+            // attach the click listener
+            attachMarkerListener(map, marker, infowindow, markerId, proxyUrl);
+            markers[i] = marker;
+    	}
+    };
+
+    // refresh the markers display
+    var overlayMarkers = function() {
+
+    	// calculate a bounding box slightly bigger
+    	// than the existing viewport
+    	var map_bounds = map.getBounds();
+
+    	var sw = map_bounds.getSouthWest();
+    	var ne = map_bounds.getNorthEast();
+
+    	var new_sw = new google.maps.LatLng(sw.lat()-0.002, sw.lng()-0.002);
+    	var new_ne = new google.maps.LatLng(ne.lat()+0.002, ne.lng()+0.002);
+    	
+    	var sub_bounds = new google.maps.LatLngBounds(new_sw,new_ne);
+    	
+    	// step through the markers showing and
+    	// hiding as necessary
+    	for ( var j = 0; j < markers.length; ++j) {
+           if ( sub_bounds.contains(markers[j].getPosition()) ) {
+        	   if(!markers[j].getVisible()) { // only show if not already visible
+        		   markers[j].setMap(map);
+        		   markers[j].setVisible(true);
+        	   }
+           } else {
+        	   markers[j].setMap(null); // removes from map
+    		   markers[j].setVisible(false);
+           }
+        }
+
+    };
+
+	findLocation = function() {
 
         // keep track of location calls
         var locationCount = 0;
@@ -147,66 +208,81 @@ var initializeMap = function(mapElementId, defaultLatitude, defaultLongitude, ma
     var hideSearchMessage = function () {
         document.getElementById("searching").style.visibility = "hidden";
     };
-
-    var overlayMarkers = function() {
-    	
-    	var infowindow = new google.maps.InfoWindow();
-
-    	for(var i=0; i<markers.length; i++) {
-        	var point = new google.maps.LatLng(markers[i].lat, markers[i].lng);
-            var markerId = markers[i].id;
-            var marker = new google.maps.Marker({
-                position: point, 
-                map: map,
-                icon: icon
-            });
-
-            attachListener(map, marker, infowindow, markerId, proxyUrl);
-    	}
-    };
     
 };
 
-var attachListener = function(map, mark, infowindow, id, url) {
+// listener for stop markers
+var attachMarkerListener = function(map, mark, infowindow, id, url) {
+	
     google.maps.event.addListener(mark, 'click', function() {
+
     	// the style should ensure the autopan pans far enough to allow
     	// space for later dynamic content
     	infowindow.setContent("<div id='transport-info-init'>Fetching...</div>");
     	infowindow.open(map,mark);
+
+    	// start the process of fetching the live info
     	getDepartureInfo(infowindow, id, url);
+
     });
+
 }
 
+// ajax request for live departure info
+var getDepartureInfo = function(infowindow, id, url) {
+	
+	var xmlhttp;
+	
+	if (window.XMLHttpRequest) {
+
+		// code for IE7+, Firefox, Chrome, Opera, Safari
+		xmlhttp=new XMLHttpRequest();
+
+	} else {
+
+		// code for IE6, IE5
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+
+	}
+	
+	xmlhttp.onreadystatechange=function() {
+
+		if(xmlhttp.readyState==4) {
+			
+			// set the contents of the infowindow
+			setInfoContent(infowindow, xmlhttp.responseText);
+
+		}
+		
+	}
+
+	// make the request
+	xmlhttp.open("GET",url+id,true);
+	xmlhttp.send(null);
+
+}
+
+// set the contents of the infowindow
 var setInfoContent = function(infowindow, json) {
+	
+	// slurp the incoming json
 	var depInfo = eval('(' + json + ')');
+
 	var content = "<div id='transport-info'>";
+
 	if(!depInfo.stop.name) {
 		content += "Sorry. No departure data is currently available for this location.";
 	} else {
+
 		content += depInfo.stop.name + " at " + depInfo.base_time;
+		
 		for(var i=0; i<depInfo.departures.length && i != 6; i++) {
 			content += "<br/>" + depInfo.departures[i].service + " <em>" + depInfo.departures[i].destination + "</em> " + depInfo.departures[i].due;
 		}
 	}
+	
 	content += "</div>";
+	
 	infowindow.setContent(content);
 }
 
-var getDepartureInfo = function(infowindow, id, url) {
-	var xmlhttp;
-	if (window.XMLHttpRequest) {
-		// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp=new XMLHttpRequest();
-	} else {
-		// code for IE6, IE5
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	
-	xmlhttp.onreadystatechange=function() {
-		if(xmlhttp.readyState==4) {
-			setInfoContent(infowindow, xmlhttp.responseText);
-		}
-	}
-	xmlhttp.open("GET",url+id,true);
-	xmlhttp.send(null);
-}
