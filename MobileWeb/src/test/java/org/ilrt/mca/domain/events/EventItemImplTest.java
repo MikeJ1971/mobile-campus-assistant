4ac5d69652ee4f9b09e23cbef234f2480bb46bfe
Chris Bailey <c.bailey@bristol.ac.uk>
2009-11-05 17:31:55 +0000

    starting on supporting repeating dates

---
+++ MobileWeb/src/test/java/org/ilrt/mca/domain/events/EventItemImplTest.java
@@ -0,0 +1,97 @@
+/*
+ *  © University of Bristol
+ */
+
+package org.ilrt.mca.domain.events;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Chris Bailey (c.bailey@bristol.ac.uk)
+ */
+public class EventItemImplTest {
+
+    private static String DATE_FORMAT_STRING = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+    
+    public EventItemImplTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+
+    @Test
+    public void testIsRecurring() throws ParseException {
+        EventItemImpl item = new EventItemImpl();
+
+        Date startdate = new SimpleDateFormat(DATE_FORMAT_STRING).parse("2009-09-28T08:00:00Z");
+
+        Date until = new SimpleDateFormat(DATE_FORMAT_STRING).parse("2009-12-11T08:00:00Z");
+
+        item.setId("2rds35n4nan737jahku8cgq3nc@google.com");
+        item.setLabel("ASSL: Opening times");
+        item.setDescription("Issue Desk Service:<P>\nMonday to Wednesday  -  8.45am - 8.00pm<br>\n\nThursday - 9.45am-8.00pm<br>\n\nFriday - 8.45am - 6.00pm <br>\n\nSaturday - 8.45 am - 6.00pm <br>\n\nSunday - No Issue Desk services<br>\n");
+        item.setStartOfWeek("MO");
+        item.setInterval(1);
+        item.setUntil(until);
+
+        System.out.println(item.isRecurring());
+        assertFalse(item.isRecurring());
+
+        item.setFrequency("DAILY");
+        assertTrue(item.isRecurring());
+
+    }
+
+    @Test
+    public void testGetRecurringDates() throws ParseException {
+        EventItemImpl item = new EventItemImpl();
+        List<Date> recurringDates;
+
+        Date startdate = new SimpleDateFormat(DATE_FORMAT_STRING).parse("2009-09-28T08:00:00Z");
+
+        Date until = new SimpleDateFormat(DATE_FORMAT_STRING).parse("2009-12-11T08:00:00Z");
+
+        item.setId("2rds35n4nan737jahku8cgq3nc@google.com");
+        item.setLabel("ASSL: Opening times");
+        item.setDescription("Issue Desk Service:<P>\nMonday to Wednesday  -  8.45am - 8.00pm<br>\n\nThursday - 9.45am-8.00pm<br>\n\nFriday - 8.45am - 6.00pm <br>\n\nSaturday - 8.45 am - 6.00pm <br>\n\nSunday - No Issue Desk services<br>\n");
+        item.setStartDate(startdate);
+        item.setStartOfWeek("MO");
+        item.setFrequency("DAILY");
+//        item.setByMonth("10");
+        item.setInterval(1);
+        item.setUntil(until);
+
+        assertTrue(item.isRecurring());
+
+        recurringDates = item.getRecurringDatesUntil(until);
+        assertEquals("Number of generated dates don't match",74,recurringDates.size());
+
+        // add a month restriction
+        item.setByMonth("1,4,11");
+        recurringDates = item.getRecurringDatesUntil(until);
+        assertEquals("Number of generated dates don't match",30,recurringDates.size());
+
+        item.setByMonth("12,9");
+        recurringDates = item.getRecurringDatesUntil(until);
+        assertEquals("Number of generated dates don't match",13,recurringDates.size());
+
+        item.setByMonth("11");
+        recurringDates = item.getRecurringDatesUntil(until);
+        assertEquals("Number of generated dates don't match",30,recurringDates.size());
+    }
+
+}
