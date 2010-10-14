6880836115377779e3a93f697b77e75ac6e6f511
Mike Jones <mike.a.jones@bristol.ac.uk>
2010-10-14 16:41:28 +0100

    comporator will order by string or label ... still needs to handle dates

--- MobileWeb/mca-jena-freemarker/src/main/java/org/ilrt/mca/freemarker/JenaResourceComparator.java
+++ MobileWeb/mca-jena-freemarker/src/main/java/org/ilrt/mca/freemarker/JenaResourceComparator.java
@@ -1,38 +1,75 @@
 package org.ilrt.mca.freemarker;
 
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.vocabulary.DC;
+import com.hp.hpl.jena.vocabulary.RDFS;
+import freemarker.template.TemplateModel;
 import org.ilrt.mca.vocab.MCA_REGISTRY;
 
 import java.util.Comparator;
 
-public class JenaResourceComparator implements Comparator {
+public class JenaResourceComparator implements Comparator<TemplateModel> {
 
     @Override
-    public int compare(Object o1, Object o2) {
+    public int compare(TemplateModel o1, TemplateModel o2) {
 
+        // make sure that we have two objects we can compare
         if (o1 instanceof ResourceTemplate && o2 instanceof ResourceTemplate) {
 
+            // get the underlying resource
             Resource r1 = ((ResourceTemplate) o1).getResource();
             Resource r2 = ((ResourceTemplate) o2).getResource();
 
-            // do we have an "mca:order"
+            // do we have an "mca:order" - this should take priority
+            if (r1.hasProperty(MCA_REGISTRY.order) && r2.hasProperty(MCA_REGISTRY.order)) {
 
-            if (r1.hasProperty(MCA_REGISTRY.order) || r2.hasProperty(MCA_REGISTRY.order)) {
-
-                // we can only compare literals
+                // they should be Literal values, but check anyway
                 RDFNode node1 = r1.getProperty(MCA_REGISTRY.order).getObject();
                 RDFNode node2 = r2.getProperty(MCA_REGISTRY.order).getObject();
 
                 if (node1.isLiteral() && node2.isLiteral()) {
-                    return node1.asLiteral().getLexicalForm()
-                            .compareTo(node2.asLiteral().getLexicalForm());
+                    int val = compareLexicalForm(node1.asLiteral(), node2.asLiteral());
+                    return val == 0 ? compareOnLabel(r1, r2) : val;
                 }
 
-            }
+                // TODO check on dates
 
+            } else { // other wise try and order on a label
+
+                return compareOnLabel(r1, r2);
+            }
         }
 
-        return 0;
+        return 0; // give up!
     }
+
+    private int compareLexicalForm(Literal a, Literal b) {
+        return a.getLexicalForm().compareTo(b.getLexicalForm());
+    }
+
+    private int compareOnLabel(Resource a, Resource b) {
+
+        Literal literal1 = getLabel(a);
+        Literal literal2 = getLabel(b);
+
+        if (literal1 != null && literal2 != null) {
+            return literal1.getLexicalForm().compareTo(literal2.getLexicalForm());
+        } else {
+            return 0;
+        }
+    }
+
+    private Literal getLabel(Resource r) {
+
+        if (r.hasProperty(RDFS.label)) {
+            return r.getProperty(RDFS.label).getLiteral();
+        } else if (r.hasProperty(DC.title)) {
+            return r.getProperty(DC.title).getLiteral();
+        }
+
+        return null;
+    }
+
 }
--- MobileWeb/mca-jena-freemarker/src/test/java/org/ilrt/mca/freemarker/JenaResourceComparatorTest.java
+++ MobileWeb/mca-jena-freemarker/src/test/java/org/ilrt/mca/freemarker/JenaResourceComparatorTest.java
@@ -4,13 +4,11 @@ import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
 import com.hp.hpl.jena.vocabulary.DC;
+import com.hp.hpl.jena.vocabulary.RDFS;
 import freemarker.template.SimpleSequence;
-import freemarker.template.TemplateModel;
 import freemarker.template.TemplateModelException;
 import org.ilrt.mca.vocab.MCA_REGISTRY;
 import org.junit.Test;
-
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,43 +17,88 @@ public class JenaResourceComparatorTest {
     @Test
     public void test() throws TemplateModelException {
 
-
         Model model = ModelFactory.createDefaultModel();
         Resource resource = model.createResource("http://example.org");
 
-        Resource lvl1 = model.createResource("http://example.org/1");
-        model.add(model.createStatement(lvl1, DC.title, "First Item"));
+        Resource lvl1 = model.createResource(uriOne);
+        model.add(model.createStatement(lvl1, DC.title, labelOne));
         model.add(model.createLiteralStatement(lvl1, MCA_REGISTRY.order, 1));
 
-        Resource lvl2 = model.createResource("http://example.org/2");
-        model.add(model.createStatement(lvl2, DC.title, "Second Item"));
+        Resource lvl2 = model.createResource(uriTwo);
+        model.add(model.createStatement(lvl2, DC.title, labelTwo));
         model.add(model.createLiteralStatement(lvl2, MCA_REGISTRY.order, 2));
 
-        Resource lvl3 = model.createResource("http://example.org/3");
-        model.add(model.createStatement(lvl3, DC.title, "Third Item"));
+        Resource lvl3 = model.createResource(uriThree);
+        model.add(model.createStatement(lvl3, DC.title, labelThree));
         model.add(model.createLiteralStatement(lvl3, MCA_REGISTRY.order, 3));
+
+        Resource lvl4 = model.createResource(uriFour);
+        model.add(model.createStatement(lvl4, DC.title, labelFour));
+        model.add(model.createLiteralStatement(lvl4, MCA_REGISTRY.order, 3));
+
+        Resource lvl5 = model.createResource(uriFive);
+        model.add(model.createStatement(lvl5, RDFS.label, labelFive));
+        model.add(model.createLiteralStatement(lvl5, MCA_REGISTRY.order, 3));
 
         model.add(model.createStatement(resource, MCA_REGISTRY.hasItem, lvl1));
         model.add(model.createStatement(resource, MCA_REGISTRY.hasItem, lvl2));
         model.add(model.createStatement(resource, MCA_REGISTRY.hasItem, lvl3));
+        model.add(model.createStatement(resource, MCA_REGISTRY.hasItem, lvl4));
+        model.add(model.createStatement(resource, MCA_REGISTRY.hasItem, lvl5));
 
         model.write(System.out);
 
         ResourceHashModel resourceHashModel = new ResourceHashModel(resource);
 
         SimpleSequence sequence = (SimpleSequence) resourceHashModel.get("mca:hasItem");
+        sequence.setObjectWrapper(new JenaObjectWrapper());
 
+        assertEquals("There should be three items in the sequence", 5, sequence.size());
 
-        assertEquals("There should be three items in the sequence", 3, sequence.size());
+        // get the first element
+        ResourceHashModel elementOne = (ResourceHashModel) sequence.get(0);
+        assertEquals("Unexpected URI", uriOne, elementOne.getAsString());
+        Resource r1 = elementOne.getResource();
+        assertEquals("Unexpected label", labelOne, r1.getProperty(DC.title).getLiteral().getLexicalForm());
 
-        List list = sequence.toList();
+        // get the second element
+        ResourceHashModel elementTwo = (ResourceHashModel) sequence.get(1);
+        assertEquals("Unexpected URI", uriTwo, elementTwo.getAsString());
+        Resource r2 = elementTwo.getResource();
+        assertEquals("Unexpected label", labelTwo, r2.getProperty(DC.title).getLiteral().getLexicalForm());
 
-        for (Object o : list) {
-            System.out.println(o);
-        }
-   
+        // get the third element
+        ResourceHashModel elementThree = (ResourceHashModel) sequence.get(2);
+        assertEquals("Unexpected URI", uriThree, elementThree.getAsString());
+        Resource r3 = elementThree.getResource();
+        assertEquals("Unexpected label", labelThree, r3.getProperty(DC.title).getLiteral().getLexicalForm());
 
+        // get the fourth element
+        ResourceHashModel elementFour = (ResourceHashModel) sequence.get(3);
+        assertEquals("Unexpected URI", uriFour, elementFour.getAsString());
+        Resource r4 = elementFour.getResource();
+        assertEquals("Unexpected label", labelFour, r4.getProperty(DC.title).getLiteral().getLexicalForm());
+
+        // get the fifth element
+        ResourceHashModel elementFive = (ResourceHashModel) sequence.get(4);
+        assertEquals("Unexpected URI", uriFive, elementFive.getAsString());
+        Resource r5 = elementFive.getResource();
+        assertEquals("Unexpected label", labelFive, r5.getProperty(DC.title).getLiteral().getLexicalForm());
     }
 
+    private final String uriOne = "http://example.org/1";
+    private final String labelOne = "Item 1";
+
+    private final String uriTwo = "http://example.org/2";
+    private final String labelTwo = "Item 2";
+
+    private final String uriThree = "http://example.org/3";
+    private final String labelThree = "Item 3";
+
+    private final String uriFour = "http://example.org/4";
+    private final String labelFour = "Item 4";
+
+    private final String uriFive = "http://example.org/5";
+    private final String labelFive = "Item 5";
 
 }
