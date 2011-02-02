a976d9459cdb3813b71e95cbc80080e077a55914
Mike Jones <mike.a.jones@bristol.ac.uk>
2011-02-02 14:59:53 +0000

    Added service module and started support for LDAP

--- MobileWeb/mca-services/src/main/java/org/ilrt/mca/services/ldap/BasicLdapSearch.java
+++ MobileWeb/mca-services/src/main/java/org/ilrt/mca/services/ldap/BasicLdapSearch.java
@@ -3,6 +3,7 @@ package org.ilrt.mca.services.ldap;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Resource;
+<<<<<<< HEAD
 import com.hp.hpl.jena.vocabulary.RDFS;
 import com.hp.hpl.jena.vocabulary.VCARD;
 import org.apache.commons.codec.binary.Hex;
@@ -19,10 +20,26 @@ import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+=======
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.vocabulary.RDFS;
+import com.hp.hpl.jena.vocabulary.VCARD;
+import org.apache.log4j.Logger;
+import org.ilrt.mca.vocab.FOAF;
+
+import javax.naming.Context;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.Attributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchResult;
+>>>>>>> 3749458... Added service module and started support for LDAP
 import java.util.Hashtable;
 
 public class BasicLdapSearch {
 
+<<<<<<< HEAD
 
     public BasicLdapSearch(Hashtable<Object, Object> env) {
 
@@ -84,6 +101,78 @@ public class BasicLdapSearch {
 
         // TOO - URI for the person?
         Resource resource = m.createResource(personUri);
+=======
+    public static void main(String[] args) {
+
+        String filter = "(cn=Mike Jones)";
+
+        Hashtable<String, String> env = new Hashtable<String, String>();
+        env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+        env.put(Context.PROVIDER_URL, "ldaps://ldap-srv.bris.ac.uk:636");
+        env.put(Context.SECURITY_PROTOCOL, "SSL");
+        env.put(Context.SECURITY_AUTHENTICATION, "simple");
+
+
+        try {
+            BasicLdapSearch basicLdapSearch = new BasicLdapSearch(env);
+            basicLdapSearch.search(filter);
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public BasicLdapSearch(Hashtable<String, String> env) throws NamingException {
+
+        this.env = env;
+
+
+    }
+
+
+    private Model search(String filter) throws NamingException {
+
+
+        Model m = ModelFactory.createDefaultModel();
+
+        log.info("Connecting to LDAP server.");
+        DirContext ctx = new InitialDirContext(env);
+
+
+        NamingEnumeration<SearchResult> results =
+                ctx.search("cn=Users,dc=bris,dc=ac,dc=uk", filter, null);
+
+
+        while (results.hasMore()) {
+
+            SearchResult result = results.nextElement();
+
+            Resource resource = createContact(result);
+
+            if (resource != null) {
+                m.add(resource.getModel());
+            }
+
+
+        }
+
+
+        m.write(System.out);
+
+        return null;
+    }
+
+    private Resource createContact(SearchResult result) throws NamingException {
+
+        Attributes attributes = result.getAttributes();
+
+        String uid = (String) attributes.get("uid").get();
+
+        System.out.println(result.getName());
+
+
+        Resource resource = ModelFactory.createDefaultModel().createResource();
+>>>>>>> 3749458... Added service module and started support for LDAP
 
         // name
         if (attributes.get("displayName") != null) {
@@ -97,6 +186,10 @@ public class BasicLdapSearch {
             resource.addLiteral(VCARD.TITLE, title);
         }
 
+<<<<<<< HEAD
+=======
+
+>>>>>>> 3749458... Added service module and started support for LDAP
         // organizational unit
         if (attributes.get("ou") != null) {
             String ou = (String) attributes.get("ou").get();
@@ -117,12 +210,17 @@ public class BasicLdapSearch {
         // email
         if (attributes.get("mail") != null) {
             String mail = (String) attributes.get("mail").get();
+<<<<<<< HEAD
             resource.addLiteral(FOAF.mbox, mail);
+=======
+            resource.addLiteral(VCARD.EMAIL, mail);
+>>>>>>> 3749458... Added service module and started support for LDAP
         }
 
         // telephone number
         if (attributes.get("telephoneNumber") != null) {
             String tel = (String) attributes.get("telephoneNumber").get();
+<<<<<<< HEAD
             resource.addProperty(FOAF.phone, handleTelephone(m, tel));
         }
 
@@ -138,6 +236,26 @@ public class BasicLdapSearch {
         r.addProperty(RDFS.label, telNumber);
 
         return r;
+=======
+            handleTelephone(tel);
+            resource.addLiteral(VCARD.TEL, tel);
+            resource.addProperty(FOAF.phone, handleTelephone(tel));
+        }
+        
+        return resource;
+    }
+
+    private Resource handleTelephone(String telNumber) {
+
+        telNumber = telNumber.replace(" ", "");
+        telNumber = telNumber.replace("(0)", "");
+
+        Resource r = ResourceFactory.createProperty("tel:" + telNumber);
+        r.addProperty(RDFS.label, telNumber);
+
+
+        return null;
+>>>>>>> 3749458... Added service module and started support for LDAP
     }
 
 
